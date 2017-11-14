99080956f72684e2178f69e0833e36a5cb7df27a
shikhar7dec <shikhar7dec@gmail.com>
2017-11-14 23:34:15 +0530

    Minor Layout Changes

--- app/src/main/java/in/goods24/home/HomeUserActivity.java
+++ app/src/main/java/in/goods24/home/HomeUserActivity.java
@@ -3,23 +3,36 @@ package in.goods24.home;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.TabHost;
+import android.widget.TableRow;
 import android.widget.Toast;
 
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 import in.goods24.common.ChangePwdActivity;
 import in.goods24.common.MainActivity;
 import in.goods24.R;
 import in.goods24.common.UpdateProfile;
 import in.goods24.common.ViewProfile;
 import in.goods24.util.ConstantsUtil;
+import in.goods24.util.HttpUtils;
 
 public class HomeUserActivity extends AppCompatActivity {
     private int backButtonCount= 0;
+    private String[] prodCatTypeArr;
+    private String[] prodCatTypeIDArr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +40,90 @@ public class HomeUserActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home_user);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarHome);
         setSupportActionBar(toolbar);
-        SharedPreferences sharedpreferences = getSharedPreferences(ConstantsUtil.MyPREFERENCES, Context.MODE_PRIVATE);
+        fetchProductCatTypes();
+
+
+
+        /*SharedPreferences sharedpreferences = getSharedPreferences(ConstantsUtil.MyPREFERENCES, Context.MODE_PRIVATE);
         String userTypeName =  sharedpreferences.getString("loginUserTypeName","");
         TextView userTypeTextView= (TextView)findViewById(R.id.userTypeTextView);
-        userTypeTextView.setText(userTypeName);
+        userTypeTextView.setText(userTypeName);*/
+    }
 
+    private void setTabs() {
+        TableRow tabs = (TableRow)findViewById(R.id.upperMenuBarRow);
+        for(int i=0;i<prodCatTypeArr.length;i++){
+            Button element = new Button(this);
+            element.setText(prodCatTypeArr[i]);
+            element.setBackgroundResource(R.drawable.button_design_background);
+            element.setTextSize(10);
+            element.setTextColor(Color.WHITE);
+            element.setId(100*(Integer.parseInt(prodCatTypeIDArr[i])+1));
+            tabs.addView(element);
+        }
+
+    }
+
+    private void fetchProductCatTypes() {
+        RequestParams requestParams =new RequestParams();
+        requestParams.add("appId", "g24API7");
+        requestParams.add("pwd", "API7g24");
+        requestParams.add("oprn","getProductType");
+        String phpName = "manageProducts.php";
+        Log.d("REQ","Request parameter are>>>>"+requestParams+">>"+phpName);
+        makeFetchCatDetailsRestCall(requestParams,phpName);
+    }
+
+    private void makeFetchCatDetailsRestCall(RequestParams requestParams, String phpName) {
+
+        HttpUtils.post(phpName, requestParams, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject res) {
+                Log.d("res", "---------------response from host>>> " + res + "Status is" + statusCode);
+                try {
+
+                    String respMsg = (String) res.getString("res");
+                    int errCode = (Integer) res.getInt("error_code");
+                    String catIds = (String)res.getString("category_id");
+                    String catNames = (String)res.getString("category_name");
+                    Log.d("res", "Response message is>>>>" + respMsg+"CatNames>>"+catNames);
+                    fetchingDetailsIntoArray(respMsg,errCode,catIds,catNames);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    showValidationMsg("Please check your internet and try again");
+                        /*RelativeLayout relLayoutProgress = (RelativeLayout) findViewById(R.id.progressBarLayout);
+                        relLayoutProgress.setVisibility(View.GONE);*/
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString,
+                                  Throwable throwable) {
+                Log.d("log", "Status code is>>" + statusCode + "Response code>>>" + responseString);
+                showValidationMsg("Some Error occurred please try again");
+                    /*RelativeLayout relLayoutProgress = (RelativeLayout) findViewById(R.id.progressBarLayout);
+                    relLayoutProgress.setVisibility(View.GONE);*/
+            }
+        });
+
+
+    }
+
+    private void fetchingDetailsIntoArray(String respMsg, int errCode, String catIds, String catNames) {
+        if(0==errCode){
+            prodCatTypeArr = catNames.split("#");
+            prodCatTypeIDArr = catIds.split("#");
+            setTabs();
+        }
+        else{
+            showValidationMsg(respMsg);
+        }
+    }
+
+    private void showValidationMsg(String message) {
+        Toast.makeText(getApplicationContext(),message,
+                Toast.LENGTH_LONG)
+                .show();
     }
 
 
